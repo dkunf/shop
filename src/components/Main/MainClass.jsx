5c0c85b0f8f43f9fe6b9014b60a0474aa17b013e
doniklt <dkunfchaworl@gmail.com>
2024-02-09 18:48:32 +0200

    switched from flex to css column prop

--- src/components/Main/MainClass.jsx
+++ src/components/Main/MainClass.jsx
@@ -16,13 +16,13 @@ class Main extends React.Component {
     const { data } = this.state;
 
     const handleSortData = (isAscending = 1) => {
-      if (isAscending !== 1 || isAscending !== -1) {
+      if (isAscending !== 1 && isAscending !== -1) {
         console.log("hendleSortData accepts only numbers 1 or -1");
         isAscending = 1;
       }
       const sortedData = data.sort((a, b) => {
         if (a.title > b.title) return isAscending;
-        if (a.title < b.title) return -isAscending;
+        if (a.title < b.title) return -1 * isAscending;
         return 0;
       });
       this.setState({
@@ -31,21 +31,23 @@ class Main extends React.Component {
     };
     //next i6kelti button i atskyra komponenta
     return (
-      <main className="main-container">
+      <>
+        {" "}
         <div className="filtrai">
           <button onClick={() => handleSortData(1)}>Sort A-Z</button>
           <button onClick={() => handleSortData(-1)}>Sort Z-A</button>
         </div>
-
-        {data.map((el) => (
-          <Card
-            src={el.src || ""}
-            key={el.title}
-            title={el.title}
-            description={el.description}
-          />
-        ))}
-      </main>
+        <main className="main-container">
+          {data.map((el) => (
+            <Card
+              src={el.src || ""}
+              key={el.title}
+              title={el.title}
+              description={el.description}
+            />
+          ))}
+        </main>
+      </>
     );
   }
 }
