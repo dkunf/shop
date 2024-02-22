fa81d5e1302d8fd411cbb8449df580e3c22f2ba9
doniklt <dkunfchaworl@gmail.com>
2024-02-22 20:17:54 +0200

    imported pics

--- src/mockData.js
+++ src/mockData.js
@@ -1,103 +1,122 @@
+import Avocado from "./files/avocado.webp";
+import Kiwi from "./files/kiwi.avif";
+import Pineapple from "./files/pineapple.avif";
+import Zucchini from "./files/zucchini.avif";
+import Cherry from "./files/cherry.avif";
+import Watermelon from "./files/watermelon.avif";
+import Cucumber from "./files/cucumber.webp";
+import Blueberry from "./files/blueberry.avif";
+import Potato from "./files/potato.avif";
+import Lettuce from "./files/lettuce.avif";
+import Strawberry from "./files/strawberry.avif";
+import Spinach from "./files/spinach.avif";
+import Apple from "./files/apple.png";
+import Banana from "./files/banana.avif";
+import Orange from "./files/orange.avif";
+import Carrot from "./files/carrot.avif";
+import Broccoli from "./files/broccoli.avif";
+import Grapes from "./files/grapes.avif";
+import Tomato from "./files/tomato.avif";
+
 export const mockData = [
   {
     title: "Apple",
     description: "Experience the crisp sweetness of fresh apples.",
-    src: "http://localhost:3232/apples.avif",
+    src: Apple,
   },
 
   {
     title: "Banana",
+    src: Banana,
     description: "Fuel your day with our potassium-packed bananas.",
-    src: "http://localhost:3232/banana.avif",
   },
   {
     title: "Orange",
+    src: Orange,
     description: "Unleash a burst of citrus delight in every bite.",
-    src: "http://localhost:3232/orange.avif",
   },
   {
     title: "Carrot",
+    src: Carrot,
     description: "Indulge in the crunchiness of nutrient-rich carrots.",
-    src: "http://localhost:3232/carrot.avif",
   },
   {
     title: "Broccoli",
+    src: Broccoli,
     description: "Elevate your health with our versatile broccoli.",
-    src: "http://localhost:3232/broccoli.avif",
   },
   {
     title: "Grapes",
+    src: Grapes,
     description: "Savor the sweetness of our bountiful grape clusters.",
-    src: "http://localhost:3232/grapes.avif",
   },
   {
     title: "Tomato",
+    src: Tomato,
     description: "Add flavor to your dishes with our juicy tomatoes.",
-    src: "http://localhost:3232/tomato.avif",
   },
   {
     title: "Spinach",
+    src: Spinach,
     description: "Revitalize your meals with our iron-packed spinach.",
-    src: "http://localhost:3232/spinach.avif",
   },
   {
     title: "Strawberry",
+    src: Strawberry,
     description: "Delight in the sweetness of succulent strawberries.",
-    src: "http://localhost:3232/strawberry.avif",
   },
   {
     title: "Cucumber",
+    src: Cucumber,
     description: "Stay refreshed with the crispness of our cucumbers.",
-    src: "http://localhost:3232/cucucmber.webp",
   },
   {
     title: "Kiwi",
+    src: Kiwi,
     description: "Boost your day with the vitamin C richness of kiwi.",
-    src: "http://localhost:3232/kiwi.avif",
   },
   {
     title: "Avocado",
+    src: Avocado,
     description: "Savor the creamy goodness of nutrient-dense avocados.",
-    src: "http://localhost:3232/avocado.webp",
   },
   {
     title: "Pineapple",
+    src: Pineapple,
     description: "Escape to the tropics with our sweet pineapples.",
-    src: "http://localhost:3232/pineapple.avif",
   },
   {
     title: "Bell Pepper",
     description: "Color your meals with the crunch of bell peppers.",
-    src: "http://localhost:3232/pepper.avif",
   },
   {
     title: "Watermelon",
+    src: Watermelon,
     description: "Quench your thirst with the juicy freshness of watermelon.",
-    src: "http://localhost:3232/watermelon.avif",
   },
   {
     title: "Cherry",
+    src: Cherry,
     description: "Indulge in the sweetness of plump, red cherries.",
-    src: "http://localhost:3232/cherry.avif",
   },
   {
     title: "Zucchini",
+    src: Zucchini,
     description: "Transform your recipes with the versatility of zucchini.",
-    src: "http://localhost:3232/zuccini.avif",
   },
   {
     title: "Blueberry",
+    src: Blueberry,
     description: "Enjoy a burst of antioxidants with our blueberries.",
-    src: "http://localhost:3232/blueberry.avif",
   },
   {
     title: "Potato",
+    src: Potato,
     description: "Ignite your culinary creativity with our versatile potatoes.",
-    src: "http://localhost:3232/potato.avif",
   },
   {
     title: "Lettuce",
+    src: Lettuce,
     description: "Create vibrant salads with our crisp and fresh lettuce.",
-    src: "http://localhost:3232/lettuce.avif",
   },
 ];
